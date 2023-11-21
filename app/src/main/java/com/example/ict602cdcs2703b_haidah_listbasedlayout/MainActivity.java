7b2e27174716ec492991837f21598980057cf2e1
haidahrahim26 <haidah266000@gmail.com>
2023-11-21 18:51:15 +0800

    My commit Lab2 ListBasedLayout

---
+++ app/src/main/java/com/example/ict602cdcs2703b_haidah_listbasedlayout/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ict602cdcs2703b_haidah_listbasedlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        String[] values = new String[]
+                {
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "Jun",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                        "December"
+                };
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1,
+                android.R.id.text1,values);
+        listView.setAdapter(adapter);
+    }
+}
